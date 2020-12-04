d07723b39a2f1b13ea830ae483c235da247c4b09
Vlad Tkachuk <tkvlads@gmail.com>
2020-12-04 11:00:52 +0200

    added export fonts

---
+++ src/fonts/HelveticaNowDisplay/index.ts
@@ -0,0 +1,13 @@
+type FontType = {
+  thin: string;
+  light: string;
+  normal: string;
+  bold: string;
+};
+
+export const font: FontType = {
+  thin: 'HelveticaNowDisplayThin',
+  light: 'HelveticaNowDisplayLight',
+  normal: 'HelveticaNowDisplay',
+  bold: 'HelveticaNowDisplayMedium',
+};
