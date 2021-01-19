cc175aad980dfe318621163294d54eac64a50818
Vlad Tkachuk <tkvlads@gmail.com>
2021-01-19 15:45:13 +0200

    created hooks profile and user

---
+++ src/redux/user/hooks.ts
@@ -0,0 +1,12 @@
+import { useSelector } from 'react-redux';
+import { RootState } from '..';
+
+export const selectUserId = (state: RootState) => state.user.userId;
+
+export const useUserId = () =>
+  useSelector((state: RootState) => {
+    const userId = state.user?.userId ?? null;
+    return userId;
+  });
+
+export const useUserAccount = () => useSelector((state: RootState) => state.user.userAccount);
