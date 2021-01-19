44cac411470827896c43293162f52438e8068314
Vlad Tkachuk <tkvlads@gmail.com>
2021-01-19 15:47:27 +0200

    added redux-profile

--- src/redux/profile/reducer.ts
+++ src/redux/profile/reducer.ts
@@ -1,45 +1,79 @@
 import { createReducer } from 'reduxsauce';
-import { UserType } from '../../services/api/api.types';
+import { UserIdProfileType } from '../../services/api/api.types';
 import {
+  GetUserProfileAction,
+  GetUserProfileFailureAction,
+  GetUserProfileSuccessAction,
   profileActionTypes,
-  GetUserAccountAction,
-  GetUserAccountSuccessAction,
-  GetUserAccountFailureAction,
+  UpdateUserProfileAction,
+  UpdateUserProfileFailureAction,
+  UpdateUserProfileSuccessAction,
 } from './actions';
 
 export interface ProfileState {
   isLoading: boolean;
-  userAccount: UserType | null;
   error: string | null;
+  // userProfile: ProfileType;
+  userProfiles: UserIdProfileType | null;
 }
+
+// const userProfiles = {'3': {skills: 'sdf', _id: 'gk', workExperience: null, education: null, hoursPerWeek: 8, rate: 3 }}
 
 const INITIAL_STATE: ProfileState = {
   isLoading: false,
   error: null,
-  userAccount: null,
+  userProfiles: null,
 };
 
 type Handler<A> = (state: ProfileState, action: A) => ProfileState;
 
-const getUserAccount: Handler<GetUserAccountAction> = (state) => ({
+const getUserProfile: Handler<GetUserProfileAction> = (state) => ({
   ...state,
   isLoading: true,
 });
 
-const getUserAccountSuccess: Handler<GetUserAccountSuccessAction> = (state, { userAccount }) => ({
+const getUserProfileSuccess: Handler<GetUserProfileSuccessAction> = (
+  state,
+  { userId, profile },
+) => {
+  return {
+    ...state,
+    isLoading: false,
+    userProfiles: { ...state.userProfiles, [userId]: profile },
+  };
+};
+
+const getUserProfileFailure: Handler<GetUserProfileFailureAction> = (state, { error }) => ({
   ...state,
   isLoading: false,
-  userAccount,
+  error: error,
 });
 
-const getUserAccountFailure: Handler<GetUserAccountFailureAction> = (state, { error }) => ({
+const updateUserProfile: Handler<UpdateUserProfileAction> = (state) => ({
+  ...state,
+  isLoading: true,
+});
+
+const updateUserProfileSuccess: Handler<UpdateUserProfileSuccessAction> = (
+  state,
+  { userProfile },
+) => ({
+  ...state,
+  isLoading: false,
+  userProfile: userProfile,
+});
+
+const updateUserProfileFailure: Handler<UpdateUserProfileFailureAction> = (state, { error }) => ({
   ...state,
   isLoading: false,
   error,
 });
 
 export const profileReducer = createReducer<ProfileState>(INITIAL_STATE, {
-  [profileActionTypes.GET_USER_ACCOUNT]: getUserAccount,
-  [profileActionTypes.GET_USER_ACCOUNT_SUCCESS]: getUserAccountSuccess,
-  [profileActionTypes.GET_USER_ACCOUNT_FAILURE]: getUserAccountFailure,
+  [profileActionTypes.GET_USER_PROFILE]: getUserProfile,
+  [profileActionTypes.GET_USER_PROFILE_SUCCESS]: getUserProfileSuccess,
+  [profileActionTypes.GET_USER_PROFILE]: getUserProfileFailure,
+  [profileActionTypes.UPDATE_USER_PROFILE]: updateUserProfile,
+  [profileActionTypes.UPDATE_USER_PROFILE_SUCCESS]: updateUserProfileSuccess,
+  [profileActionTypes.UPDATE_USER_PROFILE_FAILURE]: updateUserProfileFailure,
 });
