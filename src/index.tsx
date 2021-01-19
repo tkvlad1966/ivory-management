946ab143f7d602d40fe0a8fcbbf9767628e474d5
Vlad Tkachuk <tkvlads@gmail.com>
2021-01-19 16:04:04 +0200

    added presistor

--- src/index.tsx
+++ src/index.tsx
@@ -2,14 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import { store } from './redux/index';
+import { persistor, store } from './redux/index';
 import reportWebVitals from './reportWebVitals';
-
+import { PersistGate } from 'redux-persist/es/integration/react';
 import { Provider } from 'react-redux';
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
+      <PersistGate persistor={persistor} />
       <App />
     </Provider>
   </React.StrictMode>,
