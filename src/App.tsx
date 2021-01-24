89435119d8324139b7c076e75ed22d71d8c63b25
Vlad Tkachuk <tkvlads@gmail.com>
2021-01-24 11:59:59 +0200

    refactor App for RootNavigation and RootAuthorization

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,13 @@
 import React, { FC } from 'react';
-import styled from 'styled-components';
-import Login from './screens/auth-screen/login';
-import { BrowserRouter, Redirect, Route } from 'react-router-dom';
-import Employee from './screens/user-screen/RootNavigation';
+import { BrowserRouter } from 'react-router-dom';
+import User from './screens/user-screen/RootNavigation';
 import './utils/translation/i18n';
-import { useInitialized } from './redux/user';
+import RootAuthorization from './screens/auth-screen/RootAuthorization';
 
 const App: FC = () => {
   const token: string = localStorage.token;
-  const initialized = useInitialized();
-  console.log('initialized:', initialized);
-  return (
-    <BrowserRouter>
-      <Container>
-        {!token ? <Redirect to={'/login'} /> : <Redirect to={'/employee/home'} />}
-        <Route path="/login" render={() => <Login />} />
-        <Route path="/employee/home" render={() => <Employee />} />
-      </Container>
-    </BrowserRouter>
-  );
-};
 
-const Container = styled.div``;
+  return <BrowserRouter>{token ? <User /> : <RootAuthorization />}</BrowserRouter>;
+};
 
 export default App;
